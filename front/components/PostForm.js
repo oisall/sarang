e104c9aff6b67edf54f1309c7f093eaa9b3a9606
oisall <a@a.com>
2022-12-02 14:09:56 +0900

    sssssssss

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -1,98 +1,86 @@
-import { Form, Input, Button } from 'antd';
-import  React,{ useCallback, useEffect, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
+import { Button, Form, Input } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
+
+import { ADD_POST_REQUEST, UPLOAD_IMAGES_REQUEST, REMOVE_IMAGE } from '../reducers/post';
+import useInput from '../hooks/useInput';
 import { backUrl } from '../config/config';
-import { addPost,UPLOAD_IMAGES_REQUEST,REMOVE_IMAGE, ADD_POST_REQUEST } from '../reducers/post';
 
 const PostForm = () => {
-    const [text,setText] = useState('');
-    const dispatch = useDispatch();
-    const { imagePaths,addPostDone } = useSelector((state) => state.post);
-    const imageInput = useRef();
-    useEffect(()=>{
+  const { imagePaths, addPostDone } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+  const [text, onChangeText, setText] = useInput('');
 
-        if(addPostDone){
-            setText('');
-        }
-    },[addPostDone])
+  useEffect(() => {
+    if (addPostDone) {
+      setText('');
+    }
+  }, [addPostDone]);
 
+  const onSubmit = useCallback(() => {
+    if (!text || !text.trim()) {
+      return alert('게시글을 작성하세요.');
+    }
+    const formData = new FormData();
+    imagePaths.forEach((p) => {
+      formData.append('image', p);
+    });
+    formData.append('content', text);
+    return dispatch({
+      type: ADD_POST_REQUEST,
+      data: formData,
+    });
+  }, [text, imagePaths]);
 
+  const imageInput = useRef();
+  const onClickImageUpload = useCallback(() => {
+    imageInput.current.click();
+  }, [imageInput.current]);
 
-    const onChangeText =useCallback((e)=>{
-        setText(e.target.value);
-    })
-
-
-    const onSubmit = useCallback(() => {
-        if(!text || !text.trim()){
-            return alert('게시글을 작성하세요.');
-        }
-
-        const formData = new FormData();
-        imagePaths.forEach((p)=>{
-            formData.append('image',p);
-
-        });
-        formData.append('content',text);
-
-      return  dispatch({
-            type:ADD_POST_REQUEST,
-            data:formData
-        })
-       
-    }, [text,imagePaths]);
-
-    const onClickImageUpload=useCallback(()=>{
-imageInput.current.click();
-    },[imageInput.current])
-
-const onChangeImages = useCallback((e)=>{
-    console.log('images',e.target.files);
+  const onChangeImages = useCallback((e) => {
+    console.log('images', e.target.files);
     const imageFormData = new FormData();
-    [].forEach.call(e.target.files,(f)=>{
-        imageFormData.append('image',f);
+    [].forEach.call(e.target.files, (f) => {
+      imageFormData.append('image', f);
     });
     dispatch({
-        type:UPLOAD_IMAGES_REQUEST,
-        data:imageFormData
-    })
-})
+      type: UPLOAD_IMAGES_REQUEST,
+      data: imageFormData,
+    });
+  }, []);
 
-const onRemoveImage=useCallback((index)=>()=>{
+  const onRemoveImage = useCallback((index) => () => {
     dispatch({
-        type: REMOVE_IMAGE,
-        data:index
-    })
-})
-    return (
-        <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
-            <Input.TextArea
-                value={text}
-                onChange={onChangeText}
-                maxLength={140}
-                placeholder='어떤 신기한 일이 있었나요?'
-            />
+      type: REMOVE_IMAGE,
+      data: index,
+    });
+  }, []);
+
+  return (
+    <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
+      <Input.TextArea
+        value={text}
+        onChange={onChangeText}
+        maxLength={140}
+        placeholder="어떤 신기한 일이 있었나요?"
+      />
+      <div>
+        <input type="file" name="image" multiple hidden ref={imageInput} onChange={onChangeImages} />
+        <Button onClick={onClickImageUpload}>이미지 업로드</Button>
+        <Button type="primary" style={{ float: 'right' }} htmlType="submit">짹짹</Button>
+      </div>
+      <div>
+        {imagePaths.map((v, i) => (
+          <div key={v} style={{ display: 'inline-block' }}>
+            <img src={`${backUrl}/${v}`} style={{ width: '200px' }} alt={v} />
             <div>
-                <input type='file' name='image' multiple hidden ref={imageInput} onChange={onChangeImages} />
-                <Button onClick={onClickImageUpload}>이미지 업로드</Button>
-                <Button type='primary' style={{ float: 'right' }} htmlType='submit'>짹짹</Button>
-
+              <Button onClick={onRemoveImage(i)}>제거</Button>
             </div>
-            <div>
-                {imagePaths.map((v,i) => (
-                    <div key={v} style={{ display: 'inline-block' }}>
-                        <img src={`${backUrl}/${v}`} style={{ width: '200px' }} alt={v} />
-                        <div>
-                            <Button onClick={onRemoveImage(i)}>제거</Button>
-                        </div>
-
-
-                    </div>
-                ))}
-            </div>
-        </Form>
-    );
-
-}
+          </div>
+        ))}
+      </div>
+    </Form>
+  );
+};
 
 export default PostForm;
--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,66 +1,59 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useState } from 'react';
 import PropTypes from 'prop-types';
-import { PlusOutlined} from '@ant-design/icons';
-import ImagesZoom from "./ImagesZoom";
-import { backUrl } from "../config/config";
+import { PlusOutlined } from '@ant-design/icons';
 
+import ImagesZoom from './ImagesZoom';
+import { backUrl } from '../config/config';
 
 const PostImages = ({ images }) => {
+  const [showImagesZoom, setShowImagesZoom] = useState(false);
 
-    const [showImagesZoom, setShowImagesZoom] = useState(false);
-    const onZoom = useCallback(() => {
-        setShowImagesZoom(true);
-    }, [])
+  const onZoom = useCallback(() => {
+    setShowImagesZoom(true);
+  }, []);
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
 
-    const onClose  = useCallback(()=>{
-        setShowImagesZoom(false)
-    },[])
-
-    if (images.length === 1) {
-        return (
-            <>
-                <img role='presentation' scr={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-                {showImagesZoom && <ImagesZoom  images={images} onClose={onClose}/>}
-            </>
-        )
-    }
-    if (images.length === 2) {
-        return (
-            <>
-                <img role='presentation' style={{ width: '50%', display: 'inline-block' }} scr={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-                <img role='presentation' style={{ width: '50%', display: 'inline-block' }} scr={`${backUrl}/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
-                {showImagesZoom && <ImagesZoom  images={images} onClose={onClose}/>}
-
-            </>
-        )
-    }
+  if (images.length === 1) {
     return (
-        <>
-            <div>
-                <img role='presentation' width="50%" scr={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
-                <div
-                    role="presentation"
-                    style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
-                    onClick={onZoom}
-                >
-                    <PlusOutlined />
-                    <br />
-                    {images.length - 1}
-                    개의 사진 더보기
-
-
-                </div>
-            </div>
-            {showImagesZoom && <ImagesZoom  images={images} onClose={onClose}/>}
-
-        </>
-    )
-}
-
-
+      <>
+        <img role="presentation" src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  if (images.length === 2) {
+    return (
+      <>
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <img role="presentation" style={{ width: '50%', display: 'inline-block' }} src={`${backUrl}/${images[1].src}`} alt={images[1].src} onClick={onZoom} />
+        {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+      </>
+    );
+  }
+  return (
+    <>
+      <div>
+        <img role="presentation" style={{ width: '50%' }} src={`${backUrl}/${images[0].src}`} alt={images[0].src} onClick={onZoom} />
+        <div
+          role="presentation"
+          style={{ display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign: 'middle' }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}
+          개의 사진 더보기
+        </div>
+      </div>
+      {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </>
+  );
+};
 
 PostImages.propTypes = {
-    images: PropTypes.arrayOf(PropTypes.object),
-}
+  images: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default PostImages;
