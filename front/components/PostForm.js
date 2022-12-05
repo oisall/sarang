328cc350276898958c5b84d98ca7c9db7783eee1
oisall <a@a.com>
2022-12-05 11:37:14 +0900

    sgg

--- front/components/PostForm.js
+++ front/components/PostForm.js
@@ -81,7 +81,7 @@ const onRemoveImage=useCallback((index)=>()=>{
             <div>
                 {imagePaths.map((v,i) => (
                     <div key={v} style={{ display: 'inline-block' }}>
-                        <img src={v} style={{ width: '200px' }} alt={v} />
+                        <img src={`${backUrl}/${v}`} style={{ width: '200px' }} alt={v} />
                         <div>
                             <Button onClick={onRemoveImage(i)}>제거</Button>
                         </div>
