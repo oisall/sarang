ad56a43e8c52e4f839f6487918e9dd864505e266
oisall <a@a.com>
2022-12-06 18:14:49 +0900

    sggsssssssssssssssssssssSssss

--- front/components/UserProfile.js
+++ front/components/UserProfile.js
@@ -1,39 +1,37 @@
-import React, { useCallback } from "react";
+import React, { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Card, Avatar, Button } from 'antd';
-import {useDispatch, useSelector} from 'react-redux';
-import { logoutRequestAction } from "../reducers/user";
-import Link from "next/link";
+import Link from 'next/link';
+
+import { logoutRequestAction } from '../reducers/user';
 
 const UserProfile = () => {
-    const dispatch = useDispatch();
-    const {me,logOutLoading} = useSelector((state)=>state.user);
-const onLogOut = useCallback(()=>{
+  const dispatch = useDispatch();
+  const { me, logOutLoading } = useSelector((state) => state.user);
+
+  const onLogOut = useCallback(() => {
     dispatch(logoutRequestAction());
-})
+  }, []);
 
-    return (
-        <Card
-            actions={[
-                <div key="twit"><Link href={`/user/${me.id}`}><a>짹쨱<br />{me.Posts.length}</a></Link></div>,
-                <div key="followings"><Link href={'/profile'}><a>followings<br />{me.Followings.length}</a></Link></div>,
-                <div key="followers"><Link href={'/profile'}><a>followers<br />{me.Followers.length}</a></Link></div>,
-            ]}
-        >
-            <Card.Meta
-                title={me.nickname}
-                avatar={(
-                    <Link href={`/user/${me.id}`} prefetch={false}>
-                        <a><Avatar>{me.nickname[0]}</Avatar></a>
-                    </Link>
-                    )}
-            />
-            <Button onClick={onLogOut} loading={logOutLoading}>로그아웃</Button>
-        </Card>
-
-
-    );
-}
-
-
+  return (
+    <Card
+      actions={[
+        <div key="twit"><Link href={`/user/${me.id}`}><a>짹짹<br />{me.Posts.length}</a></Link></div>,
+        <div key="followings"><Link href="/profile"><a>팔로잉<br />{me.Followings.length}</a></Link></div>,
+        <div key="followings"><Link href="/profile"><a>팔로워<br />{me.Followers.length}</a></Link></div>,
+      ]}
+    >
+      <Card.Meta
+        avatar={(
+          <Link href={`/user/${me.id}`} prefetch={false}>
+            <a><Avatar>{me.nickname[0]}</Avatar></a>
+          </Link>
+        )}
+        title={me.nickname}
+      />
+      <Button onClick={onLogOut} loading={logOutLoading}>로그아웃</Button>
+    </Card>
+  );
+};
 
 export default UserProfile;
